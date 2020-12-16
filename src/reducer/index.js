630a247a7c404249d7cd45b2a0de8a63bc1d9ec1
LordAvorio <gadingramjets@gmail.com>
2020-12-16 16:15:46 +0700

    Gading Ujian Front End JCWM Jakarta

---
+++ src/reducer/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux'
+
+import userReducer from './userReducer'
+import {historyReducer} from './historyReducer'
+
+const allReducers =  combineReducers({
+    user: userReducer,
+    history: historyReducer
+
+})
+
+export default allReducers
